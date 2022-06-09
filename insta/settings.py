c576677b33cbffff33fb83f291391b0b6552cde7
mercyakuku <mercyakuku24@gmail.com>
2022-06-09 21:45:24 +0300

    Added cloudinary and required imports

--- insta/settings.py
+++ insta/settings.py
@@ -10,7 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
